96a43bdc3a70084c22c4c1c8446dbeccd5990d87
Alyssa <heimlichera@bzz.ch>
2022-05-17 17:08:45 +0200

    Generic DataHandler

---
+++ src/main/java/com/example/projecttracker/services/HelloApplication.java
@@ -0,0 +1,55 @@
+package com.example.projecttracker.services;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+    private static Properties properties;
+
+    /**
+     * Gets the value of a property
+     *
+     * @param property the key of the property to be read
+     * @return the value of the property
+     */
+    public static String getProperty(String property) {
+        if (HelloApplication.properties == null) {
+            setProperties(new Properties());
+            readProperties();
+        }
+        String value = HelloApplication.properties.getProperty(property);
+        if (value == null) return "";
+        return value;
+    }
+
+    /**
+     * reads the properties file
+     */
+    private static void readProperties() {
+
+        if (properties == null) {
+            properties = new Properties();
+            try (InputStream input = new FileInputStream(HelloApplication.class.getClassLoader().getResource("project.properties").getFile())) {
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Sets the properties
+     *
+     * @param properties the value to set
+     */
+    private static void setProperties(Properties properties) {
+        HelloApplication.properties = properties;
+    }
+}
