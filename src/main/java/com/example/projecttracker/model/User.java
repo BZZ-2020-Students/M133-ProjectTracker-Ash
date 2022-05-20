08c832a324eb77cb7459e790456bfd980fe08398
Alyssa <heimlichera@bzz.ch>
2022-05-21 00:08:03 +0200

    Java Docs

--- src/main/java/com/example/projecttracker/model/User.java
+++ src/main/java/com/example/projecttracker/model/User.java
@@ -1,24 +1,42 @@
 package com.example.projecttracker.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+/**
+ * The User class
+ * @author Alyssa Heimlicher
+ * @version 1.0
+ * @since 2022-05-20
+ */
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    /**
+     * The user's unique uuid
+     * @since 1.0
+     */
     private String userUUID;
+    /**
+     * The user's username
+     * @since 1.0
+     */
     private String userName;
+    /**
+     * The user's password
+     * @since 1.0
+     */
     private String password;
+    /**
+     * The Users role in the application
+     * @since 1.0
+     */
     private String userRole;
 
-    public User() {
-    }
-
-    public User(String userUUID, String userName, String password, String userRole) {
-        this.userUUID = userUUID;
-        this.userName = userName;
-        this.password = password;
-        this.userRole = userRole;
-    }
 
 }
