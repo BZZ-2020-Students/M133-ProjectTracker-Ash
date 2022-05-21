1966b571f6ae9690e7e79df716af59b67d16444c
Alyssa <heimlichera@bzz.ch>
2022-05-21 23:59:10 +0200

    created json files

--- src/main/java/com/example/projecttracker/data/DataHandlerGen.java
+++ src/main/java/com/example/projecttracker/data/DataHandlerGen.java
@@ -13,6 +13,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/**
+ * reads and writes the data in the JSON-files
+ * @author Alyssa Heimlicher
+ * @version 1.0
+ * @since 2022-05-20
+ *
+ * @param <T>
+ */
+
 @Getter
 @Setter
 @RequiredArgsConstructor
@@ -20,6 +29,12 @@ public class DataHandlerGen<T> {
     @NonNull
     private final Class<T> tClass;
 
+    /**
+     * reads all the data from the JSON-file
+     * @param propertyName the name of the property that tells us which JSON-file to read
+     * @return an ArrayList of the data
+     * @throws IOException when the file cannot be read/is not found
+     */
     public ArrayList<T> getGenericJSON(String propertyName) throws IOException {
         String filePath = Config.getProperty(propertyName);
         byte[] jsonData = Files.readAllBytes(
@@ -29,6 +44,17 @@ public class DataHandlerGen<T> {
         return objectMapper.readValue(jsonData, objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, tClass));
     }
 
+    /**
+     * reads specific data from the JSON-file
+     * @param propertyName the name of the property that tells us which JSON-file to read
+     * @param keyName the name of the key that we want to read
+     * @param key
+     * @param <Key> the type of Object that is getting read
+     * @return the data that was read
+     * @throws IOException when the file cannot be read/is not found
+     * @throws NoSuchFieldException when the field cannot be found
+     * @throws IllegalAccessException when the field cannot be accessed
+     */
     public <Key>T getSingleFromJsonArray(String propertyName, String keyName, Key key) throws IOException, NoSuchFieldException, IllegalAccessException {
         ArrayList<T> arrayList = getGenericJSON(propertyName);
 
